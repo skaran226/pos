9f451f7a1704ce7bee86b7527fd2b97cce839b9a
skaran226 <40988367+skaran226@users.noreply.github.com>
2018-09-10 17:53:21 +0530

    Update Form1.cs

--- pos_read/Form1.cs
+++ pos_read/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.Threading;
+using System.Collections;
+using System.Diagnostics;
 
 namespace pos_read
 {
@@ -21,45 +24,133 @@ namespace pos_read
             fm = this;
         }
 
+        String str = "";
+       // int j = 1;
+        string trns_no = "";
+        string reg_id = "";
+        string time = "";
+        string hot_key = "";
+
+
+        ArrayList hot_arr = new ArrayList();
+        
+
         delegate void SetRichTextValue(object sender, SerialDataReceivedEventArgs e);
         SerialPort sp = null;
         private void button1_Click(object sender, EventArgs e)
         {
+            hot_arr.Add("monst");
+            hot_arr.Add("monst spec");
+            hot_arr.Add("monst spec coffee");
+
             sp = new SerialPort();
             sp.PortName = textBox1.Text;
             sp.BaudRate = 9600;
             sp.StopBits = StopBits.One;
             sp.Parity = Parity.None;
             sp.DataBits = 8;
-            sp.DataReceived += new SerialDataReceivedEventHandler(pos_datarecived);
             sp.Open();
+
+            
+                sp.DataReceived += new SerialDataReceivedEventHandler(pos_datarecived);
+            
+            
         }
 
         private void pos_datarecived(object sender, SerialDataReceivedEventArgs e)
         {
-            int iBytesToRead = fm.sp.BytesToRead;
-            byte[] comBuffer = new byte[iBytesToRead];
-            int i;
-            // byte[] buffer = new byte[readByte];
 
-            fm.sp.Read(comBuffer, 0, iBytesToRead);
+                //ArrayList arr = new ArrayList();
+                int iBytesToRead = fm.sp.BytesToRead;
+                byte[] comBuffer = new byte[iBytesToRead];
+                int i;
+                // byte[] buffer = new byte[readByte];
+
+                    
+                    fm.sp.Read(comBuffer, 0, iBytesToRead);
 
 
-            foreach (byte ch in comBuffer)
-            {
-                if (fm.richTextBox1.InvokeRequired)
+               Thread.Sleep(50);
+                foreach (byte ch in comBuffer)
                 {
+                    
 
-                    SetRichTextValue st = new SetRichTextValue(pos_datarecived);
-                    fm.Invoke(st, new object[] { sender, e });
+                        if (fm.richTextBox1.InvokeRequired)
+                        {
+
+                            SetRichTextValue st = new SetRichTextValue(pos_datarecived);
+                            fm.Invoke(st, new object[] { sender, e });
+
+                        }
+                        else
+                        {
+                           
+                            //fm.richTextBox1.Text += (char)ch;
+                            str += (char)ch;
+                            
+                        }
+
+                       
+                }
+
+
+
+
+
+                if (str != "") {
+
+                    String[] sp_arr = str.Split(' ');
+
+                    for (i = 0; i < sp_arr.Length; i++)
+                    {
+
+                        if (i != 0)
+                        {
+                            richTextBox1.Text += "[" + i + "]:" + sp_arr[i] + "\n";
+                            //Debug.Write("[" + i + "]:" + sp_arr[i] + "\n");
+                            if (sp_arr[i].ToString() == "!")
+                            {
+                                time = sp_arr[i + 1].ToString() + " "+sp_arr[i+2].ToString()+"\n";
+                                
+                                //break;
+                            }
+
+                            if (sp_arr[i].ToString() == "TRAN#")
+                            {
+                                trns_no = sp_arr[i+1].ToString();
+                                
+                                
+                            }
+
+                        }
+
+                    }
+
+                    foreach (string s in hot_arr) {
+
+
+
+                        if (str.ToLower().Contains(s)) {
+
+                            hot_key = s ;
+                        }
+                    }
+
+                    reg_id = trns_no.Substring(0, 3);
+
+                    Console.Write(time);
+                    Console.Write(trns_no + "\n");
+                    Console.Write(hot_key + "\n");
+                    Console.Write(reg_id + "\n");
+                    str = "";
 
                 }
-                else
-                {
-                    fm.richTextBox1.Text += (char)ch;
-                }
+                
 
-            }
+                
+                    
+            
+                
         }
     }
 }
